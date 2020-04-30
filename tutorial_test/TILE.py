913cf4c12bb2a94969dd637566f9bf49e298b36f
Leeyoujin <ujin2021@naver.com>
2020-05-01 00:32:50 +0900

    youjin_updateCharacterMoving

--- tutorial_test/TILE.py
+++ tutorial_test/TILE.py
@@ -1,6 +1,7 @@
 import pygame
 
 TILESIZE = 70
+#CHARSIZE = 45
 
 class Map(pygame.sprite.Sprite):
     def __init__(self, image_name, col, row):
@@ -12,8 +13,3 @@ class Map(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.grid_x
         self.rect.y = self.grid_y
-
-class Shadow(pygame.sprite.Sprite):
-    # SHADOWSIZE =
-    def __init__(self, image_name, col, row):
-        pygame.sprite.Sprite.__init__(self)
--- tutorial_test/main_loop.py
+++ tutorial_test/main_loop.py
@@ -15,14 +15,18 @@ BLUE = (0, 0, 255)
 
 map = ["tut_map.txt", "stage1_map.txt"]
 mapTxt = [("q", "a", "z"), ("q", "a", "z")] # (big, small_top, small_bottom, shadow)
-mapImage = [("tutMapBig", "tutMapStop", "tutMapSbot"), ("s1MapBig", "s1MapStop", "s1MapSbot")] # (big, small_top, small_bottom)
+mapImage = [("tutMapBig", "tutMapStop", "tutMapS"), ("s1MapBig", "s1MapStop", "s1MapSbot")] # (big, small_top, small_bottom)
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bubble Bobble") #game title
 
 sprit_group = pygame.sprite.Group()
-
+keys = [False, False, False, False, False] # key를 눌렀는지 안눌렀는지
+char_pos = [70, 630] # player의 위치 좌표(움직일 때마다 변경해야하니까 리스트로)
+charR = pygame.transform.scale(pygame.image.load("image/charR.png"), (45, 45))
+charL = pygame.transform.scale(pygame.image.load("image/charL.png"), (45, 45))
+character = charR
 # test grid
 # def draw_grid():
 #     TILESIZE = 70
@@ -30,6 +34,15 @@ sprit_group = pygame.sprite.Group()
 #         pygame.draw.line(screen, (0,0,0,50), (x, 0), (x,HEIGHT))
 #     for y in range(0, HEIGHT, TILESIZE):
 #         pygame.draw.line(screen, (0,0,0,50), (0,y), (WIDTH, y))
+
+def get_keys(char):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        return charL
+    if keys[pygame.K_RIGHT]:
+        return charR
+    else:
+        return char
 
 # 계속 화면이 보이도록 한다
 while True:
@@ -46,17 +59,33 @@ while True:
 
     for col in range(0, len(map_data)): #세로
         for row in range(0, len(map_data[col])): #가로
-            if map_data[col][row] == mapTxt[0][0]:
-                map_big = Map(mapImage[0][0], col, row)
-                sprit_group.add(map_big)
-            if map_data[col][row] == mapTxt[0][1]:
-                map_small_top = Map(mapImage[0][1], col, row)
-                sprit_group.add(map_small_top)
-            if map_data[col][row] == mapTxt[0][2]:
-                map_small_bot = Map(mapImage[0][2], col, row)
-                sprit_group.add(map_small_bot)
-            # shadow 넣는거 추가
+            if(col == len(map_data)-1):
+                print("col:",col)
+                if map_data[col][row] == mapTxt[0][0]:
+                    map_big = Map(mapImage[0][0], col, row)
+                    sprit_group.add(map_big)
+                if map_data[col][row] == mapTxt[0][1]:
+                    map_small_top = Map(mapImage[0][1], col, row)
+                    sprit_group.add(map_small_top)
+                if map_data[col][row] == mapTxt[0][2]:
+                    map_small_bot = Map(mapImage[0][2], 9.65, row)
+                    sprit_group.add(map_small_bot)
+            else:
+                if map_data[col][row] == mapTxt[0][0]:
+                    map_big = Map(mapImage[0][0], col, row)
+                    sprit_group.add(map_big)
+                if map_data[col][row] == mapTxt[0][1]:
+                    map_small_top = Map(mapImage[0][1], col, row)
+                    sprit_group.add(map_small_top)
+                if map_data[col][row] == mapTxt[0][2]:
+                    map_small_bot = Map(mapImage[0][2], col, row)
+                    sprit_group.add(map_small_bot)
+
     sprit_group.draw(screen)
+    character = get_keys(character)
+    # sprit_group.add(character)
+    print("char pos:", char_pos)
+    screen.blit(character, char_pos)
 
     pygame.display.flip() # 화면 다시 그리기
 
@@ -64,3 +93,31 @@ while True:
         if event.type == pygame.QUIT: # x를 눌렀으면
             pygame.quit() #terminate game
             exit() #exit while loop
+        if event.type == pygame.KEYDOWN:  # key가 눌려지는 type의 event이면
+            if event.key == pygame.K_UP:  # 위쪽 방향키가 눌려지면
+                keys[0] = True
+            elif event.key == pygame.K_LEFT:  # 왼쪽 방향키가 눌려지면
+                keys[1] = True
+            if event.key == pygame.K_DOWN:  # 아래쪽 방향키가 눌려지면
+                keys[2] = True
+            if event.key == pygame.K_RIGHT:  # 오른쪽 방향키가 눌려지면
+                keys[3] = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:  # 위쪽 방향키를 떼면
+                keys[0] = False
+            elif event.key == pygame.K_LEFT:  # 왼쪽 방향키를 떼면
+                keys[1] = False
+            if event.key == pygame.K_DOWN:  # 아래쪽 방향키를 떼면
+                keys[2] = False
+            if event.key == pygame.K_RIGHT:  # 오른쪽 방향키를 떼면
+                keys[3] = False
+
+    #9 Move player
+    if keys[0]:
+        char_pos[1] = char_pos[1] - 20 # y값 감소 -> 위로이동
+    elif keys[2]:
+        char_pos[1] = char_pos[1] + 20 # y값 증가 -> 아래로 이동
+    if keys[1]:
+        char_pos[0] = char_pos[0] - 20 # x값 감소 -> 왼쪽으로 이동
+    elif keys[3]:
+        char_pos[0] = char_pos[0] + 20 # x값 증가 -> 오른쪽으로 이동
