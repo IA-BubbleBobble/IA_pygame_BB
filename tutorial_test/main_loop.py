8ec8cf56b73df59fe84dc61bd23c797adc2eb959
Leeyoujin <ujin2021@naver.com>
2020-04-30 15:14:23 +0900

    tutorial_test

--- tutorial_test/main_loop.py
+++ tutorial_test/main_loop.py
@@ -1,39 +1,59 @@
 # 1. pygame module 불러오기
 import pygame
+import TILE
 
-# 2. pygame initialize
+WIDTH = 1050
+HEIGHT = 700
+fps = 60
+
+#define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+TILESIZE = 70
+
 pygame.init()
-width, height = 1100, 700 # width, height init
-screen = pygame.display.set_mode((width, height)) # width, height넣어서 screen이라는 객체 생성
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # width, height넣어서 screen이라는 객체 생성
+pygame.display.set_caption("Bubble Bobble") #game title
 
-# 3. 이미지를 가져온다
 map_tut_big = pygame.transform.scale(pygame.image.load("image/map_tutorial_1(big).PNG"), (70, 70)) # 이 image를 가지고 객체를 만들겠다
 map_tut_small = pygame.transform.scale(pygame.image.load("image/map_tutorial_2(small).PNG"), (70, 20))
 keys = [False, False, False, False, False] # key를 눌렀는지 안눌렀는지
 player_pos = [100, 100] # player의 위치 좌표(움직일 때마다 변경해야하니까 리스트로)
 
+def draw_grid():
+    for x in range(0, WIDTH, TILESIZE):
+        pygame.draw.line(screen, (0,0,0,50), (x, 0), (x,HEIGHT))
+    for y in range(0, HEIGHT, TILESIZE):
+        pygame.draw.line(screen, (0,0,0,50), (0,y), (WIDTH, y))
+
 # 4. 계속 화면이 보이도록 한다
 while True:
     # 5. clear screen
-    screen.fill((0,0,0)) #(R,G,B)
-
+    screen.fill(WHITE)
+    # draw_grid()
     # 6. 모든 요소들을 다시그린다(풀위에 player-> 풀을 먼저 그린 후 player그린다)
     # print("small",map_tut_small.get_width()) #116
     # print(map_tut_big.get_width()) #76
     # print("small", map_tut_small.get_height()) #23
     # print(map_tut_big.get_height()) #73
+    #
+    for x in range(WIDTH):
+        screen.blit()
 
-    for x in range(width//map_tut_small.get_width() + 1):
-        screen.blit(map_tut_small, (x*70, 50))
-        screen.blit(map_tut_small, (x*70, 630))
-    for y in range(height // map_tut_big.get_height() - 2):
-        screen.blit(map_tut_big, (0, (y+1)*70))
-        screen.blit(map_tut_big, (1030, (y+1)*70))
+    # for x in range(WIDTH//map_tut_small.get_width() + 1):
+    #     screen.blit(map_tut_small, (x*70, 50))
+    #     screen.blit(map_tut_small, (x*70, 630))
+    # for y in range(HEIGHT // map_tut_big.get_height() - 2):
+    #     screen.blit(map_tut_big, (0, (y+1)*70))
+    #     screen.blit(map_tut_big, (1030, (y+1)*70))
 
-    screen.blit(map_tut_small, (210, 490))
-    for i in range(10):
-        screen.blit(map_tut_small, (70*(3+i), 420))
-    screen.blit(map_tut_small, (820, 210))
+    # screen.blit(map_tut_small, (210, 490))
+    # for i in range(10):
+    #     screen.blit(map_tut_small, (70*(3+i), 420))
+    # screen.blit(map_tut_small, (820, 210))
 #-----------------------------------------------------------------------------------------------------
     # 7. 화면을 다시 그린다.
     pygame.display.flip()
