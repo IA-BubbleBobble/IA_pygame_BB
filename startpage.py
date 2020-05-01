bce90bbd9933d18ce88af4b28cb8e7e097c089d7
chea-young <lchy0413@gmail.com>
2020-05-01 20:40:03 +0900

    cheayoung 0501 20:39

--- startpage.py
+++ startpage.py
@@ -1,16 +1,17 @@
 import pygame
 from pygame.locals import *
 import time
-import keyboard
-import sys
+from settings import *
+from sprites import *
 
 class Bubble:
     def __init__(self):
         WHITE = {255, 255,255}
         self._running = True
         self._display_surf = None
-        self.size = self.weight, self.height = 1050, 700
+        self.size = self.weight, self.height = WIDTH, HEIGHT
         self.clock = pygame.time.Clock()
+        self.score = 0
 
     def on_init(self):
         pygame.init()
@@ -19,42 +20,57 @@ class Bubble:
     
     def loadimage(self,image,location):
         load_image = pygame.image.load(image).convert()
-        self._display_surf.blit(load_image,location)   
+        self._display_surf.blit(load_image,location)
 
-    def start(self):
-        start_background = pygame.mixer.Sound("MainTheme.ogg")
-        start_background.play()
-        
+    def printword_white(self,size, word,location):
+        font = pygame.font.Font(BUBBLE_FONT,size)
+        text = font.render(word,True,WHITE)
+        self._display_surf.blit(text,location)   
 
-        self.loadimage("./image/start.png",(0,0))
-        pygame.display.flip()
+    def show_start_screen(self):
+        start_music = pygame.mixer.Sound("MainTheme.ogg")
+        start_music.play()
 
-        press = True
         color = 1
         progress_sec=0
-        while press:
+        while(True):
             progress_sec +=1
-
-            if(color == 1):
-                self.loadimage('image/pink_bubble.png',(0,0))
-                color = 0
+            self._display_surf.fill(WHITE)
+            self.loadimage(START_SCREEND,(0,0))
+            string_score = "00"
+            if(self.score !=0):
+                string_score = str(self.score)
+            self.printword_white(18,string_score,(175,56))
+            self.printword_white(18,"A.START            B.TUTORIAL",(273,600))
+            #self.printword_white(18,string_score,(490,56))
+            if(progress_sec>60):
+                if(color == 1):
+                    self.loadimage(PINK_SUPERBUBBLE,(260,70))
+                    color = 0
+                else:
+                    self.loadimage(YELLOW_SUPERBUBBLE,(260,70))
+                    color = 1
             else:
-                self.loadimage('image/yellow_bubble.png',(0,0))
-                color = 1
+                if(color == 1):
+                    self.loadimage(PINK_BUBBLE,(260,70))
+                    color = 0
+                else:
+                    self.loadimage(YELLOW_BUBBLE,(260,70))
+                    color = 1
             pygame.display.flip()
-            self.clock.tick(120)
+            self.clock.tick(FPS)
 
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_1] :
-                print('1')
-                press = False
-            elif keys[pygame.K_2] :
-                print('2')
-                press = False
+            for event in pygame.event.get():
+                if(event.type ==pygame.K_a):
+                    print("a")
+                    break
+                elif(event.type == pygame.K_b):
+                    print('b')
+                    break
             
-            if(progress_sec > 100):
-                press = False
-        start_background.stop()
+            if(progress_sec > 200):
+                break
+        start_music.stop()
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -71,14 +87,7 @@ class Bubble:
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
-
-        while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
-            self.start()
-            self.on_loop()
-            self.on_render()
-            
+        self.show_start_screen()            
         self.on_cleanup()
 
 if __name__ == "__main__" :
